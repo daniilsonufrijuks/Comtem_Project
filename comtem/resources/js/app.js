561010e6165c62ec0593ccffe7efb529668c1678
daniilsonufrijuks <do00492@rvt.lv>
2024-12-15 10:02:50 +0200

    app and blade changed, name of the web site

--- comtem/resources/js/app.js
+++ comtem/resources/js/app.js
@@ -11,10 +11,10 @@ import 'font-awesome/css/font-awesome.css';
 import store from "@/Cart/cart.js";
 
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    // title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
