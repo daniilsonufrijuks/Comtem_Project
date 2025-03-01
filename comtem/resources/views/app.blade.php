01bba6cc98aa8acecc87d857522e18fb2e39d6ab
daniilsonufrijuks <do00492@rvt.lv>
2025-03-01 17:51:47 +0200

    blade meta updated

--- comtem/resources/views/app.blade.php
+++ comtem/resources/views/app.blade.php
@@ -2,9 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta name="author" content="daniilsonufrijuks">
+    <meta name="description" content="Laptop, PC, Components shop with auction and ai">
+    <meta http-equiv="refresh" content="180">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{{--    <title inertia>{{ config('app.name', 'Laravel') }}</title>--}}
 
     <title >COMTEM</title>
 
