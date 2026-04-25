026d7a9ec4cb4911ab9c3b70904dc5cf138169e8
Daniils <do00492@rvt.lv>
2026-04-25 12:35:08 +0300

    added new migration

--- comtem/start.sh
+++ comtem/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-php artisan migrate --force --no-interaction
+php artisan migrate --force --no-interaction --path=database/migrations/2026_04_25_092457_import_initial_data_from_sql_dump.php
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
