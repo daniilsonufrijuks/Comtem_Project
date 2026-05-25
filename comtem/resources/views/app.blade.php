1e3a115656a64b9e155915a75e9fdc3471f32bf2
Daniils <do00492@rvt.lv>
2026-05-25 14:13:12 +0300

    new meta

--- comtem/resources/views/app.blade.php
+++ comtem/resources/views/app.blade.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <meta name="author" content="daniilsonufrijuks">
     <meta name="description" content="Laptop, PC, Components shop with auction and ai">
-    <meta http-equiv="refresh" content="900">
+    <meta http-equiv="refresh" content="1000">
     <meta name="keywords" content="auction, e-commerce, buy, sell, deals, online shopping, pc, laptop, components, parts, auction, ai, stores near me, hardware store near me, lv, en">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />--}}
 
 
     <title>COMTEM</title>
